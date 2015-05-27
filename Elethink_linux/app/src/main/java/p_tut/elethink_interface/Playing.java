cc1fee0e8a4e7224b06d30da5044dec1ac2f6c2b
faflo10 <faflo10>
2015-05-27 15:13:14 +0200

    FINI, ENFIN !

--- Elethink_linux/app/src/main/java/p_tut/elethink_interface/Playing.java
+++ Elethink_linux/app/src/main/java/p_tut/elethink_interface/Playing.java
@@ -7,7 +7,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -20,45 +23,31 @@ import p_tut.elethink_interface.local_database.LocalListDb;
 public class Playing extends ActionBarActivity {
     private ImageButton back;
     private TextView mot;
-    private Button i_know;
-    private Button i_dont_know;
-    private Button right;
-    private Button wrong;
+    private Button one;
+    private Button two;
+    private Button next;
+    private ViewSwitcher switch1;
     private LocalListDb db;
     private SQLiteDatabase local;
-    private int max;
     private ArrayList<String> questions;
     private ArrayList<String> answers;
+    private int value_rand;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.play_screen_1);
+        setContentView(R.layout.play_screen);
     }
 
     protected void onStart() {
         super.onStart();
-        setContentView(R.layout.play_screen_1);
-
+        setContentView(R.layout.play_screen);
         initComponents();
+        setButtons();
 
-        String nom = getIntent().getStringExtra("name");
-
-        Cursor query = local.query(nom, new String[] {"question"},null,null,null,null,null);
-        int question_t = query.getColumnIndex("question");
-        while(query.moveToNext()) {
-            questions.add(query.getString(question_t));
-        }
-        query.close();
-
-        query = local.query(nom, new String[] {"answer"},null,null,null,null,null);
-        int answer_t = query.getColumnIndex("answer");
-        while(query.moveToNext()) {
-            answers.add(query.getString(answer_t));
-        }
-        query.close();
-
+        setButtons1();
         Random id_rand = new Random();
-        mot.setText(questions.get(id_rand.nextInt(questions.size())));
+        value_rand = id_rand.nextInt(questions.size());
+        mot.setText(questions.get(value_rand));
     }
 
     protected void onDestroy() {
@@ -69,10 +58,11 @@ public class Playing extends ActionBarActivity {
     public void initComponents() {
         back = (ImageButton) findViewById(R.id.back_button);
         mot = (TextView) findViewById(R.id.mot);
-        i_know = (Button) findViewById(R.id.i_know);
-        i_dont_know = (Button) findViewById(R.id.i_dont_know);
-        right = (Button) findViewById(R.id.right);
-        wrong = (Button) findViewById(R.id.wrong);
+        one = (Button) findViewById(R.id.i_know);
+        two = (Button) findViewById(R.id.i_dont_know);
+        next = (Button) findViewById(R.id.next);
+        switch1 = (ViewSwitcher) findViewById(R.id.switch1);
+
 
         db = new LocalListDb(getBaseContext(),"list.db", null, 1);
         local = db.getWritableDatabase();
@@ -80,10 +70,108 @@ public class Playing extends ActionBarActivity {
         questions = new ArrayList<>();
         answers = new ArrayList<>();
 
+        Cursor query = local.query(getIntent().getStringExtra("name"), new String[] {"question"},
+                null,null,null,null,null);
+        int question_t = query.getColumnIndex("question");
+        while(query.moveToNext()) {
+            questions.add(query.getString(question_t));
+        }
+        query.close();
+
+        query = local.query(getIntent().getStringExtra("name"), new String[] {"answer"},
+                null,null,null,null,null);
+        int answer_t = query.getColumnIndex("answer");
+        while(query.moveToNext()) {
+            answers.add(query.getString(answer_t));
+        }
+        query.close();
+    }
+
+    public void setButtons1() {
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch1.showNext();
+                mot.setText(answers.get(value_rand));
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                one.setText("Right");
+                two.setText("Wrong");
+                setButtons2();
+                mot.setText(answers.get(value_rand));
+
+            }
+        });
+    }
+
+    public void setButtons2() {
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random id_rand = new Random();
+                if(questions.size() != 1) {
+                    int verif = value_rand;
+                    do {
+                        value_rand = id_rand.nextInt(questions.size());
+                    } while(verif == value_rand);
+                } else {
+                    value_rand = id_rand.nextInt(1);
+                }
+                one.setText("I know");
+                two.setText("I don't know");
+                setButtons1();
+                mot.setText(questions.get(value_rand));
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                questions.remove(value_rand);
+                answers.remove(value_rand);
+
+                if(questions.size() == 0) {
+                    Toast.makeText(getApplicationContext(),"Fini",Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Random rand = new Random();
+                    value_rand = rand.nextInt(questions.size());
+                    one.setText("I know");
+                    two.setText("I don't know");
+                    setButtons1();
+                    mot.setText(questions.get(value_rand));
+                }
+            }
+        });
+    }
+
+    public void setButtons() {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random id_rand = new Random();
+                if(questions.size() != 1) {
+                    int verif = value_rand;
+                    do {
+                        value_rand = id_rand.nextInt(questions.size());
+                    } while(verif == value_rand);
+                } else {
+                    value_rand = id_rand.nextInt(1);
+                }
+                switch1.showPrevious();
+                setButtons1();
+                mot.setText(questions.get(value_rand));
             }
         });
     }
